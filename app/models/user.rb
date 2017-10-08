fa052322d03c5bb43be507eb175fb209bc1eec33
Rakesh <rakesh.v@live.in>
2017-10-08 04:33:58 +0000

    add user table, model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User <ActiveRecord::Base
    
+   has_many :articles
+   
+   before_save {self.email = email.downcase }
+   
    validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}
    validates :email, presence: true, uniqueness: {case_sensitive: false}
    
